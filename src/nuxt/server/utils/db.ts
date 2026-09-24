39ecc0680d2bac504546918d8f32d9da83c6dc05
Simon Brundin <simonbrundin@gmail.com>
2026-09-24 11:55:44 +0200

    fix: increase database connection timeout

--- src/nuxt/server/utils/db.ts
+++ src/nuxt/server/utils/db.ts
@@ -9,7 +9,7 @@ export function getPool(): Pool {
       connectionString,
       max: 10,
       idleTimeoutMillis: 30000,
-      connectionTimeoutMillis: 5000,
+      connectionTimeoutMillis: 30000,
     })
   }
   return pool
